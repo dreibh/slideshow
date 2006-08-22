2a92f6a61aac5f1804a3b18deebab85e74caddf9
dreibh <dreibh@acdd8558-d4fd-0310-b0a5-8cf3c1ec824a>
2006-08-22 12:41:26 +0000

    git-svn-id: https://svn.tdr.iem.uni-due.de:8443/svn/sctp-misc/trunk/slideshow@114 acdd8558-d4fd-0310-b0a5-8cf3c1ec824a

--- src/tools.h
+++ src/tools.h
@@ -1,3 +1,18 @@
+/*
+ * XHTML 1.1 image presentation and JavaScript-based slideshow generator
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id$
+ */
+
 #ifndef TOOLS_H
 #define TOOLS_H
 
