51fbf86443dc9114717c17ff9da2b3889ff13ded
Thomas Dreibholz <dreibh@simula.no>
2017-08-09 12:31:28 +0200

    Header and URL updates.

--- src/infrastructure/slideshow.js
+++ src/infrastructure/slideshow.js
@@ -3,7 +3,7 @@
  * - XHTML 1.1 image presentation and JavaScript-based slideshow generator  -
  * --------------------------------------------------------------------------
  *
- * Copyright (C) 2003-2016 by Thomas Dreibholz
+ * Copyright (C) 2003-2017 by Thomas Dreibholz
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
